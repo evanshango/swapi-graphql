2b366ef0397e45fb7d017bc8daee2a9bed7c8381
Evans Shango <eshango@btn-solutions.co.ke>
2022-05-07 18:15:58 +0300

    initial commit

---
+++ src/test/java/com/codewithevans/swapi/SwapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithevans.swapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
